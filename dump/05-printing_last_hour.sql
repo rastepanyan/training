45d35c321d0eb3d13f6cce91b132f568650f6145
Rafi Stepanyan <rastepanyan@gmail.com>
2018-08-01 10:35:36 +0300

    New fixes2a

--- dump/05-printing_last_hour.sql
+++ dump/05-printing_last_hour.sql
@@ -7,7 +7,7 @@ WHERE date_of_creation > SUBDATE(NOW(), INTERVAL 1 HOUR);
 /* Other solutions are:
 *
 *	SELECT title 
-*   FROM products
+*	FROM products
 *	WHERE date_of_creation > DATE_SUB(NOW(), INTERVAL 1 HOUR);
 *
 *
