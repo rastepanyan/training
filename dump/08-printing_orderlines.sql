0752077b7160982c06b9d42ccaa5c85d9e71b4e9
Rafi Stepanyan <rastepanyan@gmail.com>
2018-08-02 16:13:27 +0300

    New fixes4

--- dump/08-printing_orderlines.sql
+++ dump/08-printing_orderlines.sql
@@ -1,9 +1,7 @@
 /* 	Printing the list orderlines with combined order information and user information.*/
 
-CREATE TEMPORARY TABLE orderlines_tmp SELECT * FROM orderlines;
-ALTER TABLE orderlines_tmp DROP orderlines_id;
-SELECT *
-FROM orderlines_tmp 
+SELECT order_id, item_price, product_id, quantity, position, order_date, user_id, amount, first_name, last_name, address, post_code, city, country_code 
+FROM orderlines 
 INNER JOIN orders USING(order_id) 
 INNER JOIN users USING(user_id) 
 ORDER BY order_id, position;
