0bf3d1a0e95aaddd78767c426430fd429e4b1396
Rafi Stepanyan <rastepanyan@gmail.com>
2018-08-03 16:23:40 +0300

    Add HTML

--- www/index.php
+++ www/index.php
@@ -1,37 +1,45 @@
-<?php 
-   $servername = "localhost";
-   $username = "root";
-   $password = "1234root";
-   $db = "myDB";
-   	
-   $conn = mysqli_connect($servername, $username, $password, $db);
-   	
-   if (! $conn) {
-   	die( "Failed! " . mysqli_connect_error());
-   } 
-   echo "Connection established";
-   ?>
-<html>
+<!DOCTYPE html>
+<html lang="bg-bg" xml:lang="bg-bg"
+   xmlns="http://www.w3.org/1999/xhtml">
    <head>
-      <title>Index</title>
+      <title>Shop</title>
+      <meta content="IE=edge" http-equiv="X-UA-Compatible">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta charset="utf-8">
-      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <meta content="shop, bargain, Plovdiv, price, cheap, quality" name="keywords">
+      <link href="css/lib/bootstrap.css" rel="stylesheet">
    </head>
    <body>
-      <h1>Connect</h1>
-      <?php
-         $query = "SELECT * FROM users";
-         mysqli_query($db, $query) or die('Error!');
-         
-         $result = mysqli_query($db, $query);
-         $row = mysqli_fetch_array($result);
-         
-         while ($row = mysqli_fetch_array($result)) {
-          echo $row['first_name'] . ' ' . $row['last_name'] . ': ' . $row['address'] . ' ' . $row['post_code'] . ' ' . $row['city'] . ' ' . $row['country_code'] .'<br />';
-         }
-         mysqli_close($db);
-         ?>
+      <div class="navbar-wrapper">
+         <div class="container">
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+               <div class="container">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                     <span class="sr-only"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     </button>
+                     <a class="navbar-brand" href="#">SHOP</a>
+                  </div>
+                  <div id="navbar" class="navbar-collapse collapse">
+                     <ul class="nav navbar-nav">
+                        <li class="active toscroll">
+                           <a href="#">Home</a>
+                        </li>
+                        <li>
+                           <a class="toscroll" href="plist.php">Product</a>
+                        </li>
+                        <li>
+                           <a class="toscroll" href="#">Order</a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      </div>
    </body>
 </html>
